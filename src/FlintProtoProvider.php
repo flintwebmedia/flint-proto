b833ecd15ff14910dca71b6c2f4ac26aa1bca3f3
Niels Steenman <ncsteenman@gmail.com>
2017-03-19 14:27:03 +0100

    Fix autoload

--- src/FlintProtoProvider.php
+++ src/FlintProtoProvider.php
@@ -14,7 +14,6 @@ class FlintProtoProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
-        // Test
         $this->loadViewsFrom(__DIR__.'/resources/views', 'flintproto');
     }
 
